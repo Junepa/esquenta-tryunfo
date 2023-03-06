477aa8a543054aad91bfb1e68bff2b6c00ddf43d
Juliana <junepa@gmail.com>
2023-03-06 10:15:25 -0300

    iniciando

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Form from './components/Form';
 function App() {
   return (
     <div className="App">
-     <Form buttonText="Adicionar"/>
+     <Form buttonText="Adicionar--"/>
     </div>
   );
 }
